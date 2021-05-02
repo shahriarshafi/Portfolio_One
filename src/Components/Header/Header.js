1e4361ee3efc100a1c6cad627e1240947c8dfbbd
Shahriar Rubiat <shahriarshafi@gmail.com>
2021-05-02 21:04:14 +0600

    Added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container">
-                    <a class="navbar-brand" href="#"><span class="text-warning">SHAHRIAR</span></a>
+                    <a class="navbar-brand" href="/home"><span class="text-warning">SHAHRIAR</span></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
